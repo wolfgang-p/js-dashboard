c24e28e5bf582a02b31a02ef2410b15d2f6c0dd2
Wolfgang Preinfalk <w.preinfalk@outlook.de>
2022-05-26 01:52:54 +0200

    First push

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,25 @@
+import React, { createContext, useContext, useState } from "react";
+
+const StateContext = createContext();
+
+const initialState = {
+  chat: false,
+  cart: false,
+  userProfile: false,
+  notification: false,
+};
+
+export const ContextProvider = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [isClicked, setIsClicked] = useState(initialState);
+
+  return (
+    <StateContext.Provider
+      value={{ activeMenu, setActiveMenu, isClicked, setIsClicked }}
+    >
+      {children}
+    </StateContext.Provider>
+  );
+};
+
+export const useStateContext = () => useContext(StateContext);
